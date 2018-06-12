81d203f1dd36469b8b2d41be4ad65f999c33c5d2
Eddie Harris and Swati Sahay <eddie@>
2018-06-12 16:43:51 -0700

    fixed errors

--- js/scripts.js
+++ js/scripts.js
@@ -19,25 +19,35 @@ var name;
 var initialDeposit;
 $("form#formOne").submit(function(event) {
  event.preventDefault();
+
 name = $("input#name").val();
 initialDeposit = parseInt($("input#initial").val());
 if (isNaN(initialDeposit)) {
   initialDeposit = 0;
 }
+$("#formOne").hide();
 document.getElementById("test").innerHTML = "Hi " + name + " your initial balance is: " +initialDeposit;
+
+ $(".initial-hide").show();
 });
+
 $("form#formOne1").submit(function(event) {
  event.preventDefault();
 
 var deposit = parseInt($("input#deposit").val());
+if (isNaN(deposit))  {
+  deposit = 0;
+}
 var withdrawl = parseInt($("input#withdrawl").val());
+if (isNaN(withdrawl))  {
+  withdrawl = 0;
+}
 
 
 var firstTransaction = new transaction(name, initialDeposit);
 var fistAmmount = new transAmmount(deposit, withdrawl);
 
 firstTransaction.arr.push(fistAmmount);
-
 
 
 
